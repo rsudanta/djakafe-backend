c1f89d0934dcf47aafbc921e0c1f825509a9abe4
Danta <rsudanta20@gmail.com>
2021-05-30 18:51:00 +0700

    port

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ app.use('/api',routes);
 // const port = process.env.PORT || 5000;
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log(`Listening to port ${port}`);
+    console.log(`Run`);
 });
